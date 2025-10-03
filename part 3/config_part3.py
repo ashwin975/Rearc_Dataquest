5ac60c3b1bbb8c7eb1e6a81802fd33a69844aeed
ashwin975 <104617880+ashwin975@users.noreply.github.com>
2025-10-03 16:04:23 -0500

    Add files via upload

---
+++ part 3/config_part3.py
@@ -0,0 +1,4 @@
+BLS_BUCKET_NAME = "bls-sync-data-ashwin"     
+BLS_PREFIX = "bls-datasets/pr.data.0.Current"
+API_PREFIX = "api-data/population.json"            
+API_BUCKET_NAME = "api-call-ashwin"
